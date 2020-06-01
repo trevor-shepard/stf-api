a3bdc77800e6eaba4eb3577cd6721fbab5ca1af9
trevor-shepard <trevorbshepard@gmail.com>
2020-06-01 15:18:28 -0500

    cleanup

--- controllers/seasons-controller.js
+++ controllers/seasons-controller.js
@@ -3,79 +3,109 @@ const SeasonModel = require('../models/season-model');
 const {InsufficentDataError} = require('../utils/errors');
 
 module.exports = class SeasonController {
-    constructor(admin) {
-        this.model = new SeasonModel(admin)
-        this.userModel = new UserModel(admin)
-    }
+	constructor(admin) {
+		this.model = new SeasonModel(admin);
+		this.userModel = new UserModel(admin);
+	}
 
-    async get(id) {
-        try {
-            const season = await this.model.get(id)
-        } catch (error) {
+	async get(id) {
+		try {
+			const season = await this.model.get(id);
+			return season;
+		} catch (error) {
 			console.log('controller level season get error', error);
-            throw error
-        }
-    }
+			throw error;
+		}
+	}
 
-    async create(data, uid) {
-        try {
-            const user = this.userModel.get(uid)
-            const season = this.model.create({
-                ...data,
-                users: {
-                    [uid]: user.username
-                }
-            })
+	async create(data, uid) {
+		try {
+			const user = this.userModel.get(uid);
+			const season = this.model.create({
+				...data,
+				users: {
+					[uid]: user.username
+				}
+			});
 
-            await this.userModel.update(season.id, {
-                seasons: {
-                    ...user.seasons
-                    [season.id]: season.name
-                }
-            })
+			await this.userModel.update(season.id, {
+				seasons: {
+					...user.seasons,
+					[season.id]: season.name
+				}
+			});
 
-            return season
-        } catch (error) {
+			return season;
+		} catch (error) {
 			console.log('controller level season create error', error);
-            throw error
-        }
-    }
+			throw error;
+		}
+	}
 
+	async join(uid, seasonID) {
+		try {
+			if ([uid, seasonID].includes(undefined)) {
+				throw new InsufficentDataError('missing id');
+			}
 
-    async join(uid, seasonID) {
-        try {
-            const user = this.userModel.get(uid)
-            const season = this.model.get(seasonID)
+			const user = this.userModel.get(uid);
+			const season = this.model.get(seasonID);
 
-            await this.userModel.update(uid, { seasons: { [season.id]: season.name }})
-            await this.model.update(seasonID, { users: {...season.users, [user.id]: user.username}})
-        } catch(error) {
-            console.log('controller level season join error', error);
-            throw error
-        }
-    }
+			await this.userModel.update(uid, {seasons: {[season.id]: season.name}});
+			await this.model.update(seasonID, {users: {...season.users, [user.id]: user.username}});
+		} catch (error) {
+			console.log('controller level season join error', error);
+			throw error;
+		}
+	}
 
-    async vote(seasonID, activity, uid, value) {
-        try {
-            const season = this.model.get(seasonID)
+	async vote(seasonID, activity, uid, value) {
+		try {
+			const parsedValue = Number.parseInt(value, 10);
+			if ([activity, parsedValue, uid, seasonID].includes(undefined)) {
+				throw new InsufficentDataError('missing id');
+			}
 
-            const update = JSON.parse(JSON.stringify(season.activities));
-            
-            const activities = Object.keys(season.activities)
-            if (activities[activity]) {
-                update[activity] = { ...update[activity], [uid]: value}
-            } else {
-                update[activity] = { [uid]: value } 
+			const season = this.model.get(seasonID);
 
-            }
+			const update = JSON.parse(JSON.stringify(season.activities));
 
-            await this.model.update(seasonID, {
-                activities: update
-            })
-        } catch (error) {
-            console.log('controller level season vote error', error);
-            throw error
-        }
-    }
+			const activities = Object.keys(season.activities);
+			if (activities[activity]) {
+				update[activity] = {...update[activity], [uid]: parsedValue};
+			} else {
+				update[activity] = {[uid]: parsedValue};
+			}
 
-}
+			await this.model.update(seasonID, {
+				activities: update
+			});
+		} catch (error) {
+			console.log('controller level season vote error', error);
+			throw error;
+		}
+	}
+
+	validateVote(seasonID, activity, uid, value) {
+		try {
+			this.model.validateID(seasonID);
+			this.model.validateString(activity);
+			this.model.validateID(uid);
+			this.model.validateNumber(value);
+		} catch (error) {
+			console.log('controller level season vote validation error', error);
+			throw error;
+		}
+	}
+
+	async begin(seasonID) {
+		try {
+			await this.model.update(seasonID, {
+				phase: 1
+			});
+		} catch (error) {
+			console.log('controller level season begin error', error);
+			throw error;
+		}
+	}
+};
--- models/season-model.js
+++ models/season-model.js
@@ -1,77 +1,76 @@
-const DataModel = require('./data-model')
+const DataModel = require('./data-model');
 const {NotFoundError, InsufficentDataError} = require('../utils/errors');
 
 module.exports = class SeasonModel extends DataModel {
-    constructor(admin) {
-        super(admin, 'groups');
-    }
+	constructor(admin) {
+		super(admin, 'groups');
+	}
 
-    async create(data) {
-        try {
-            this.validateData(data)
-            const {name, users, activities} = data
-
-            const payload = {
-                name,
-                users,
-                activities,
-                phase: 0,
-                users: {}
-            }
-
-            const id = this.createObject(payload)
-
-            return {
-                ...payload,
-                id
-            }
-
-        } catch (error) {
-            console.log('Get Model Level Group Error', error);
-        }
-        
-    }
-
-    async get(id) {
-        try {
-            if (!id) throw InsufficentDataError('missing id')
-
-            const season = await this.getRaw(id)
-
-            if (!season) throw NotFoundError('group not found')
-
-            return season
-        } catch (error) {
-            console.log('Get Model Level Group Error', error);
-            throw error
-        }
-    }
-
-    async update(id, update) {
+	async create(data) {
 		try {
-			if (!update || !id) throw new InsufficentDataError('insufficent data for user update');
-			
+			this.validateData(data);
+			const {name, users, activities} = data;
+
+			const payload = {
+				name,
+				users,
+				activities,
+				phase: 0
+			};
+
+			const id = this.createObject(payload);
+
+			return {
+				...payload,
+				id
+			};
+		} catch (error) {
+			console.log('Get Model Level Group Error', error);
+		}
+	}
+
+	async get(id) {
+		try {
+			if (!id) {
+				throw new InsufficentDataError('missing id');
+			}
+
+			const season = await this.getRaw(id);
+
+			if (!season) {
+				throw new NotFoundError('group not found');
+			}
+
+			return season;
+		} catch (error) {
+			console.log('Get Model Level Group Error', error);
+			throw error;
+		}
+	}
+
+	async update(id, update) {
+		try {
+			if (!update || !id) {
+				throw new InsufficentDataError('insufficent data for user update');
+			}
+
 			this.validateID(id);
 
-			await this.updateWithID(update, uid);
+			await this.updateWithID(update, id);
 		} catch (error) {
 			console.log('Update Model Level User Error', error);
 
 			throw error;
 		}
-    }
+	}
 
+	// Private methods
 
+	validateData(data) {
+		const {name, users, values} = data;
 
-    // private methods
-
-    validateData(data) {
-        const {name, users, values} = data
-        
-        this.validateString(name)
-        this.validateMapObject(users)
-        this.validateMapObject(values)
-    }
-    
-
-}
+		this.validateString(name);
+		this.validateMapObject(users);
+		this.validateMapObject(values);
+	}
+};
